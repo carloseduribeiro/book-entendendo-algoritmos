7f816ae22231f6d05303b77650310eb91a0b8149
carloseduribeiro <carloseribeiro96@gmail.com>
2024-10-29 17:55:14 -0300

    adding sum list algorithm that uses divide-and-conquer technique

---
+++ entendendo-algoritmos-portuguese-book/divide-and-conquer/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+// sum sums all elements of the list recursively and return
+func sum(list []int) int {
+	if len(list) == 0 {
+		return 0
+	}
+	return list[0] + sum(list[1:])
+}
+
+func main() {
+	fmt.Println(sum([]int{2, 4, 6}))
+}
