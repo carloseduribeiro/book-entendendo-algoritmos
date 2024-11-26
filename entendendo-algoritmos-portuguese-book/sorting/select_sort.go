1bad8c4985edb1042fb7f79dddfd9a2b503bfc32
carloseduribeiro <carloseribeiro96@gmail.com>
2024-11-26 07:27:52 -0300

    fixing select sort

--- entendendo-algoritmos-portuguese-book/sorting/select_sort.go
+++ entendendo-algoritmos-portuguese-book/sorting/select_sort.go
@@ -1,7 +1,7 @@
 package sorting
 
 // searchMinor returns the index of the lowest value in the list
-func searchMinor(list []int) *int {
+func searchMinor(list []int) int {
 	low := list[0]
 	lowIndex := 0
 	for i := 1; i < len(list); i++ {
@@ -10,17 +10,16 @@ func searchMinor(list []int) *int {
 			lowIndex = i
 		}
 	}
-	return &lowIndex
+	return lowIndex
 }
 
-// SelectSort teratively finds the smallest element in the list, appends it to a new list, and removes it from the original list until sorted. 
+// SelectSort teratively finds the smallest element in the list, appends it to a new list, and removes it from the original list until sorted.
 func SelectSort(list []int) []int {
-	new := make([]int, 0, len(list))
-	for range list {
-		lowIndex := searchMinor(list)
-		new = append(new, list[*lowIndex])
-		list = append(list[:*lowIndex], list[*lowIndex+1:]...)
+	new := make([]int, len(list))
+	copy(new, list)
+	for i := range new {
+		lowIndex := searchMinor(new[i:])
+		new[i], new[i+lowIndex] = new[i+lowIndex], new[i]
 	}
 	return new
 }
-
